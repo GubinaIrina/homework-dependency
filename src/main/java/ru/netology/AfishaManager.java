5585434d29188b719dc546b64e15e6c2087a164b
Gubina Irina <irina.gubina98@mail.ru>
2020-10-23 01:51:18 +0300

    Обновлены AfishaManager.java, AfishaManagerTestNonEmptyWithSetup.java, PurchaseItem.java

--- src/main/java/ru/netology/AfishaManager.java
+++ src/main/java/ru/netology/AfishaManager.java
@@ -3,15 +3,17 @@ package ru.netology;
 public class AfishaManager {
     private PurchaseItem[] items = new PurchaseItem[0];
     public void add(PurchaseItem item){
+
         int lenght = items.length + 1;
         PurchaseItem[] tmp = new PurchaseItem[lenght];
         for (int i = 0; i < items.length; i++) {
-           tmp[i] = items[i];
+        tmp[i] = items[i];
         }
         int lastIndex = tmp.length - 1;
         tmp[lastIndex] = item;
         items = tmp;
     }
+
     public PurchaseItem[] getAll(){
         PurchaseItem[] result = new PurchaseItem[items.length];
         for (int i = 0; i < result.length; i++) {
@@ -20,17 +22,5 @@ public class AfishaManager {
         }
          return result;
         }
-    public void removeById(int id) {
-        int lenght = items.length - 1;
-        PurchaseItem[] tmp = new PurchaseItem[lenght];
-        int index = 0;
-        for (PurchaseItem item : items) {
-            if (item.getId() != id) {
-                tmp[index] = item;
-                index++;
-            }
-        }
-        items = tmp;
-    }
 
 }
--- src/test/java/ru/netology/AfishaManagerTestNonEmptyWithSetup.java
+++ src/test/java/ru/netology/AfishaManagerTestNonEmptyWithSetup.java
@@ -7,35 +7,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class AfishaManagerTestNonEmptyWithSetup {
     private AfishaManager manager = new AfishaManager();
-    private PurchaseItem first = new PurchaseItem(1, 1, "first", 1, 1);
-    private PurchaseItem second = new PurchaseItem(2, 2, "second", 1, 1);
-    private PurchaseItem third = new PurchaseItem(3, 3, "third", 1, 1);
+    private PurchaseItem first = new PurchaseItem(1, 1, "Бладшот");
+    private PurchaseItem second = new PurchaseItem(2, 2, "Вперед");
+    private PurchaseItem third = new PurchaseItem(3, 3, "Отель 'Белград'");
+    private PurchaseItem four = new PurchaseItem(4, 4, "Джентельмены");
+    private PurchaseItem five = new PurchaseItem(5, 5, "Человек-невидимка");
+    private PurchaseItem six = new PurchaseItem(6, 6, "Тролли.Мировой тур");
+    private PurchaseItem seven = new PurchaseItem(7, 7, "Номер один");
+
+
 
     @BeforeEach
     public void setUp() {
         manager.add(first);
         manager.add(second);
         manager.add(third);
+        manager.add(four);
+        manager.add(five);
+        manager.add(six);
+        manager.add(seven);
+
     }
     @Test
-    public void shouldRemoveIfExists() {
-
-        int idRemove = 1;
-        manager.removeById(idRemove);
+    public void shouldAddIfExists() {
 
         PurchaseItem[] actual = manager.getAll();
-        PurchaseItem[] expected = new PurchaseItem[]{third, second};
-
-        assertArrayEquals(expected, actual);
-    }
-    @Test
-    public void shouldNotRemoveIfNotExists() {
-
-        int idToRemove = 4;
-        manager.removeById(idToRemove);
-
-        PurchaseItem[] actual = manager.getAll();
-        PurchaseItem[] expected = new PurchaseItem[]{third, second, first};
+        PurchaseItem[] expected = new PurchaseItem[]{seven, six, five, four, third, second, first};
 
         assertArrayEquals(expected, actual);
     }
