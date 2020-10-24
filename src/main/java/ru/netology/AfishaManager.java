3786b877336f40857bad9598f565b4e45d06cc62
Gubina Irina <irina.gubina98@mail.ru>
2020-10-24 12:37:11 +0300

    Обновлен AfishaManager.java

--- src/main/java/ru/netology/AfishaManager.java
+++ src/main/java/ru/netology/AfishaManager.java
@@ -8,25 +8,25 @@ public class AfishaManager {
     private PurchaseItem[] items = new PurchaseItem[0];
     private int lenght = 10;
 
-    public void add(PurchaseItem item){
+    public void add(PurchaseItem item) {
 
         int lenght = items.length + 1;
         PurchaseItem[] tmp = new PurchaseItem[lenght];
         for (int i = 0; i < items.length; i++) {
-        tmp[i] = items[i];
+            tmp[i] = items[i];
         }
         int lastIndex = tmp.length - 1;
         tmp[lastIndex] = item;
         items = tmp;
     }
 
-    public PurchaseItem[] getAll(int lenght){
+    public PurchaseItem[] getAll(int lenght) {
         PurchaseItem[] result = new PurchaseItem[getLenght()];
         for (int i = 0; i < result.length; i++) {
             int index = getLenght() - 1 - i;
             result[i] = items[index];
         }
-         return result;
-        }
+        return result;
+    }
 
 }
